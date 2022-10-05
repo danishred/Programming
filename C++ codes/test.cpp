4957b573b29653aa644ed20ab9e1677d50d4fb13
danishred <danishgreets@gmail.com>
2022-10-06 03:03:12 +0530

    inti

--- C++ codes/test.cpp
+++ C++ codes/test.cpp
@@ -1,18 +1,26 @@
-//
 #include <iostream>
-#include <Chrono>
 using namespace std;
-auto start = chrono::steady_clock::now();
+void fib(int n)
+{
+    int f[n];
+    int i;
+    f[0] = 0;
+    f[1] = 1;
+    for (i = 2; i < n; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+
+for (i = 0; i < n; i++)
+{
+    cout << f[i] << " ";
+}
+}
 
 int main()
 {
-      
-        
-    //---------------------------------------------------------------------
-    cout << "\n";
-    auto end = chrono::steady_clock::now();
-    auto diff = end - start;
-    cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
-    
+    int n = 10;
+    fib(n);
+    getchar();
     return 0;
 }
