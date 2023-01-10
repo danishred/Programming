c458b325dd2db22a2fa0a12fdefae4e14a211dac
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-01-10 21:46:51 +0530

    inti

--- C++ codes/test.cpp
+++ C++ codes/test.cpp
@@ -1,32 +1,20 @@
-// Collatz Conjecture - It states that it is always possible to
-// get back to '1'
+//
 #include <iostream>
 using namespace std;
 
-// Function Prototype
-int collatz(int n);
-
 int main()
 {
-    int n = 0;
-    cin >> n;
-    cout << collatz(n);
-    return 0;
-}
-
-int collatz(int n)
-{
-    int count = 0;
-    if (n == 1)
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
     {
-        return (n - 1);
+      if (i == 0 && j == 0)
+      {
+        cout << "hello";
+       
+      }
+      // cout << "hey";
     }
-    else if (n % 2 == 0)
-    {
-        return 1 + collatz(n / 2);
-    }
-    else
-    {
-        return 1 + collatz(3 * n + 1);
-    }
-}
+  }
+  return 0;
+you are me arif
