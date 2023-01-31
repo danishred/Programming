2b41ce85fd8a9c1f9d4774ffc3fe42dfed6d958f
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-01-31 09:46:29 +0530

    Init

--- GFG/Test.cpp
+++ GFG/Test.cpp
@@ -1,25 +1,34 @@
-// Count Digits
+// Factorial Iterative
+
+// Theta (n)
+// Auxilary space requirement big O (1)
 #include <iostream>
 using namespace std;
-
-int count_digit(int n)
+int factzero(int n)
 {
-
+    int fact = 1;
     int count = 0;
 
-    while (n != 0)
+    for (int i = 0; i < n; i++
     {
-        n = n / 10;
-        count++;
+        fact = fact * (i + 1);
     }
 
-    return count;
+    while (fact % 10 == 0)
+    {
+        count++;
+        fact /= 10;
+    }
+
+    return (count);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    cout << count_digit(n);
+    cout << factzero(n);
     return 0;
 }
+
+TC
