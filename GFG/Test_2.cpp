b913173d2e59893aa4a6bb2ffba5eeaa166cc245
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-02-01 07:48:52 +0530

    Init

--- GFG/Test_2.cpp
+++ GFG/Test_2.cpp
@@ -1,11 +1,63 @@
-//
+// GCD of two numbers
 
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
+
+// Naive Solution to find HCF
+
+// Time Complexity: O(min(a,b))
+// Auxiliary Space: O(1)
+
+int GCD(int n1, int n2)
+{
+    int mini = min(n1, n2);
+
+    while (mini > 0)
+    {
+        if (n1 % mini == 0 && n2 % mini == 0)
+        {
+            break;
+        }
+        mini--;
+    }
+
+    return mini;
+}
+
+// Euclidean Algorithm
+
+// Time Complexity:
+// Auxiliary Space:
+
+int gcdEu(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+
+int gcdEuOp(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcdEuOp(b, a % b);
+}
 
 int main()
 {
-    
+    int n1, n2;
+    cout << "Enter the two no:\n";
+    cin >> n1 >> n2;
+    cout << "Basic: " << GCD(n1, n2);
+    cout << "\n"
+         << "Euclidean: " << gcdEu(n1, n2);
     return 0;
 }
-
