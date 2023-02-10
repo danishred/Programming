2192ff15c6f313aba3602e433683745cbe04f6c1
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-02-10 16:47:02 +0530

    Init

--- C++ codes/test.cpp
+++ C++ codes/test.cpp
@@ -1,15 +1,18 @@
 //
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int sum = 0;
-   
-    for (int i = 1; i <= 5; i++)
-    {
-        sum += i;
-    }
-    cout<<sum;
+    int a = 10, b = 3, c;
+
+    float d;
+
+    c = a % b;
+
+    d = a / b;
+
+    printf(" Value of c and d are %d and %f respectively", c, d);
     return 0;
 }
--- Python Codes/test.py
+++ Python Codes/test.py
@@ -1,19 +1,53 @@
+# TODO
 
-# Python3 code to demonstrate
-# Type conversion in list of dicts.
-# using naive method
- 
-# initializing list of dictionary
-test_list = [{'a' : '1', 'b' : '2'}, { 'c' : '3', 'd' : '4'}]
- 
-# printing original list
-print ("The original list is : " + str(test_list))
- 
-# using naive method
-# type conversation in list of dicts.
-for dicts in test_list:
-    for keys in dicts:
-        dicts[keys] = int(dicts[keys])
-     
-# printing result
-print ("The modified converted list is : " +  str(test_list))
+# Card Number input
+Number = int(input("Number: "))
+print(Number)
+
+# Sum of the digits that were multiplied by 2
+secondsum = 0
+
+# sum of the digits that weren't multiplied by 2
+lastsum = 0
+
+#  Credit number saved for later use
+NumPreserved = Number
+
+# Digit Count
+count = 0
+
+# Checksum Calculation
+while Number:
+
+    lastsum = lastsum + Number % 10
+    Number = Number / 10
+    count+1
+    if Number % 10 < 5:
+        secondsum = secondsum + (Number % 10) * 2
+    else:
+        secondsum = secondsum + ((Number % 10) * 2) % 10 + \
+            ((Number % 10) * 2) / 10
+    Number = Number / 10
+    if Number > 0:
+        count+1
+
+# Validating Checksum
+    if (lastsum + secondsum) % 10 == 0:
+        # Checking for card length
+        if count == 13 or count == 15 or count == 16:
+            # Fetching first two Digits
+            while NumPreserved / 100 > 0:
+                NumPreserved = NumPreserved / 10
+            #  Checking starting Digits
+            if NumPreserved == 34 or NumPreserved == 37:
+                print("AMEX")
+            elif NumPreserved > 50 and NumPreserved < 56:
+                print("MASTERCARD")
+            elif NumPreserved / 10 == 4:
+                print("VISA")
+            else:
+                print("INVALID")
+        else:
+            print("INVALID")
+    else:
+        print("INVALID")
