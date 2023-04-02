78cbbea0260dfbd18198c47ebd3ebdf81882abce
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-02 21:55:31 +0530

    init

--- C++ codes/test.cpp
+++ C++ codes/test.cpp
@@ -3,44 +3,20 @@
 #include <iostream>
 using namespace std;
 
+int fun(int x)
+{
+     int y;
+     if (x > 100)
+          y = x - 10;
+     else
+          y = fun(fun(x + 11));
+     return y;
+}
+
 int main()
 {
-<<<<<<< HEAD
-    int a = 0, i = 0, b;
-    {
-        for (i = 0; i < 5; i += 0.5)
-        {
-            a++;
-            continue;
-        }
-        printf("%d", a);
 
-        return 0;
-    }
+     int z = fun(95);
+     cout << z;
+     return 0;
 }
-=======
-     int Array[3][3];
-
-     for (int i = 0; i < 3; i++)
-     {
-          for (int j = 0; j < 3; j++)
-          {
-               std::cin>>Array[i][j];
-          }
-          
-     }
-     
-     for (int i = 0; i < 3; i++)
-     {
-          for (int j = 0; j < 3; j++)
-          {
-               std::cout<<Array[i][j];
-          }
-          cout<<endl;
-     }
-
-
-     
-    return 0;
-}
->>>>>>> da096ce206387447acd1d393cac37f3bc9de3e41
