83976d1e9503abffb7c7cc7b25c681d6acb63542
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-18 14:32:39 +0530

    init

--- NPTEL/test.c
+++ NPTEL/test.c
@@ -1,13 +1,15 @@
+
+
 #include <stdio.h>
+#define A 5
+#define B 8
+#define C 2
+
 int main()
+
 {
-	short num[3][2] = {2, 5, 11, 17, 23, 28};
-	
-	//increases address//
-	printf("%d,%d,%d\n",num,(num + 1),(num + 2)); 
-	
-	//either use double dereference of brackets to get the value
-	printf("%d,%d,%d,%d,%d,%d",*(num),**(num),(num)[0],(num)[0][0],**(num + 1),*(num + 2)[0]);
-	
+
+	int(*x)[A][B][C];
+	printf("%ld", sizeof(*x));
 	return 0;
 }
