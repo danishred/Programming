029f60df2727b07db1ab77c8d59b1d851918a98e
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-18 10:42:51 +0530

    init

--- C++ codes/Pointer_concept.cpp
+++ C++ codes/Pointer_concept.cpp
@@ -11,7 +11,21 @@ int main()
     cout << *p << endl;
     *p++; // means the same, ++*p would increase value
     cout << *p << endl;
-    *p=*p+3;
+    *p = *p + 3;
     cout << *p << endl;
+
+    short num[3][2] = {2, 5, 11, 17, 23, 28};
+
+    // increases address//
+    printf("%d,%d,%d\n", num, (num + 1), (num + 2));
+
+    // either use double dereference of brackets to get the value
+    printf("%d,%d,%d,%d,%d,%d\n", *(num), **(num), (num)[0], (num)[0][0], **(num + 1), *(num + 2)[0]);
+
+    int x = 10;
+
+    // we can do the same with variable
+    printf("%d,%d,%d,%d,%d", (x), *(&x), (&x)[0], (x + 1), *(&x + 1));
+
     return 0;
 }
