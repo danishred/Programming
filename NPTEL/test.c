a4b926348112295fc92212b82f0feb9ce2f9869f
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-20 17:22:57 +0530

    init

--- NPTEL/test.c
+++ NPTEL/test.c
@@ -1,26 +1,2 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int fib(int n)
-{
-	if (n == 1)
-	{
-		return 1;
-	}
-
-	if (n == 0)
-	{
-		return 0;
-	}
-
-	return (fib(n - 1) + fib(n - 2));
-}
-
-int main()
-{
-	int n, result;
-	printf("enter value of n :\n");
-	scanf("%d", &n);
-	result = fib(n);
-	printf("%d\n", result);
-	return 0;
-}
