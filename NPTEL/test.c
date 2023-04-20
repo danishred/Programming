82077cad85949093eb7575c5af57aee7d3706a59
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-20 20:28:16 +0530

    init

--- NPTEL/test.c
+++ NPTEL/test.c
@@ -1,2 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+struct p
+{
+    int x;
+    char y;
+};
 
+int main()
+{
+    struct p p1[] = {1, 21, 69, 42, 64};
+    struct p *ptr1 = p1;
+    int x = (sizeof(p1) / 4);
+    if ((x == sizeof(int) + 2 * sizeof(char)))
+        printf("True");
+    else
+        printf("False");
+    return 0;
+}
