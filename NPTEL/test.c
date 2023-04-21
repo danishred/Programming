9ce2d85443c2e3ed84e421b3f414d9811e42da39
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-21 18:57:54 +0530

    init

--- NPTEL/test.c
+++ NPTEL/test.c
@@ -17,9 +17,21 @@ int main()
     return 0;
 }
 
-
-int checkPrime(int num, int num2)
+int checkPrime(int num, int i)
 {
-    
-
+    if (i == 1)
+    {
+        return 1;
+    }
+    else
+    {
+       if (num % i == 0)
+       {
+         return 0;
+       }
+       else
+       {
+         return checkPrime(num, i - 1);
+       }
+    }
 }
