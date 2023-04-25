c6266fe0929eff616b6bf9f3357e9ade807dbb52
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-26 05:20:18 +0530

    init

--- NPTEL/test.c
+++ NPTEL/test.c
@@ -1,37 +1,11 @@
 #include <stdio.h>
-int checkPrime(int, int); //Function to check prime or not 
 
 int main()
 {
-    int num, check;
-    scanf("%d", &num); //The number is taken from test case data
-    check = checkPrime(num, num/2);
-    if (check == 1)
-    {
-        printf("%d is a prime number", num);
-    }
+    int i=0;
+    if (++i)
+        printf("NPTEL");
     else
-    {
-        printf("%d is not a prime number", num);
-    }
-    return 0;
-}
-
-int checkPrime(int num, int i)
-{
-    if (i == 1)
-    {
-        return 1;
-    }
-    else
-    {
-       if (num % i == 0)
-       {
-         return 0;
-       }
-       else
-       {
-         return checkPrime(num, i - 1);
-       }
-    }
+        printf("Programming on C");
+    printf("%d\n", i);
 }
