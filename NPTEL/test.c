6e9e36c6aeff345a980266ba74a06446c2bb2dd6
Md Danish Akhter <83757129+danishred@users.noreply.github.com>
2023-04-26 07:23:11 +0530

    init

--- NPTEL/test.c
+++ NPTEL/test.c
@@ -1,14 +1,9 @@
-// pre and post increases value of the expression as well
 
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-   int arr[] = {1, 2, 3, 4, 5, 6};
-   int i, j, k;
-   j = ++arr[2];
-   k = arr[1]++;
-   i = arr[j++];
-   printf("%d\n%d\n%d\n%d", i, j, k, arr[2]);
-   return 0;
+   int n = 20;
+   int N = 21;
+   printf("%d-%d",n,N);
 }
