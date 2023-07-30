3c8068ed41888701865cb9712427b2d21be52b03
danishred <83757129+danishred@users.noreply.github.com>
2023-07-30 16:24:49 +0530

    init

--- DSA/Power.cpp
+++ DSA/Power.cpp
@@ -4,6 +4,21 @@
 #include "supplement.h"
 using namespace std;
 
+// Efficient
+int power_e(int x, int n)
+{
+    if (n == 0)
+        return 1;
+    int temp = power_e(x, n / 2);
+    temp = temp * temp;
+    if (n % 2 == 2)
+        return temp;
+    else
+        return temp*x;
+}
+// Time Complexity: Θ(logn)
+// Auxiliary Space: Θ(logn)
+
 // Naive
 int power_n(int x, int n)
 {
@@ -23,6 +38,7 @@ int main()
     int n, x;
     cout << "please enter a numberand its exponent\n";
     cin >> x >> n;
-    cout << power_n(x,n)<< endl;
+    cout << power_n(x, n) << endl;
+    cout << power_e(x, n) << endl;
     return 0;
 }
