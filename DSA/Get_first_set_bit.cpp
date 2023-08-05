7552620945e2a993619a5f10db06748a90bb9379
danishred <83757129+danishred@users.noreply.github.com>
2023-08-05 14:42:41 +0530

    init

---
+++ DSA/Get_first_set_bit.cpp
@@ -0,0 +1,60 @@
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        
+        int pos = 1;
+        while ((n&1)==0)
+        {
+            n=n>>1;
+            pos++;
+        }
+        return pos;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+// ALSO
+//   Time Comp - 0(1)       Space Comp - 0(1)
+
+//     unsigned int getFirstSetBit(int n)
+//     {
+//         if(n==0)
+//             return 0;
+//         return log2( n & -n) + 1; or  return log2(n & (~n+1))+1;
+//     }
