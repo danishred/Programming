f426656a52a96f571996e13a63ec2b370f03ab44
danishred <83757129+danishred@users.noreply.github.com>
2023-08-07 23:47:25 +0530

    init

--- DSA/Test.cpp
+++ DSA/Test.cpp
@@ -1,20 +1,45 @@
-// Count_set_bits
+//{ Driver Code Starts
+// Initial Template for C++
 
-#include <iostream>
-#include "supplement.h"
+#include <bits/stdc++.h>
 using namespace std;
 
+// } Driver Code Ends
+// User function Template for C++
+
+
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        int res = 0;
+        for(int i = 1; i <=n; i++)
+        {
+            int x = i;
+            while (x != 0)
+            {
+                x = x & (x - 1);
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
 int main()
 {
-    int n = 15;
-    int res = 0;
-    while (n != 0)
-    {
-        n = n & (n-1);
-        res++;
-    }
-    cout << res << endl;
+
+    int n;
+    cin >> n; // input n
+    Solution ob;
+    cout << ob.countSetBits(n) << endl; // print the answer
+
+    return 0;
 }
 
-// Time Complexity:Θ(d) | d is number of set bits
-// Auxiliary Space:O(1)
+// } Driver Code Ends
