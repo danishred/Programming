79f263165f0809c551f50d45a1e93ed9ff187766
danishred <83757129+danishred@users.noreply.github.com>
2023-08-10 16:10:52 +0530

    init

--- DSA/Bit Magic/Test.cpp
+++ DSA/Bit Magic/Test.cpp
@@ -1,106 +1,51 @@
-// Check for power of 2 using brian kanningham
+// Count odd numbers in array
 
 #include <iostream>
 #include "supplement.h"
 using namespace std;
 
-
-// Efficient solution
-void chk2p(int n)
+// Mine
+void chkodd_arr(int arr[], int n)
 {
-    if (n == 0)
+    if (n & 1 == 0)
     {
-        cout << "not divisible by 2" << endl;
-        return;
+        cout << "no odd" << endl;
     }
-    if ((n & n - 1) == 0)
-        cout << "yes" << endl;
-    else
-        cout << "no" << endl;
-}
-// Time Complexity:Θ(1)
-// Auxiliary Space:Θ(1)
 
-// Naive Solution
-void chk2n(int n)
-{
-    if (n == 0)
+    int count = 0, temp = 0;
+
+    for (int i = 0; i < n; i++)
     {
-        cout << "not divisible by 2" << endl;
-        return;
-    }
-    while (n > 1)
-    {
-        if (n % 2 != 0)
+        if (count % 2 != 0)
         {
-            cout << "not a power of 2" << endl;
+            cout << temp << endl;
             return;
         }
-        n = n / 2;
+        count = 0;
+        temp = arr[i];
+        for (int j = 0; j < n; j++)
+        {
+            if (temp == arr[j])
+            {
+                count++;
+            }
+        }
     }
-    cout << "n is a power of 2" << endl;
 }
-// Time Complexity: O(logn)
-// Auxiliary Space: O(1)
+// Time Complexity:O(logn) 
+// Auxiliary Space:O(1)
+
+
 
 int main()
 {
     int n;
     cin >> n;
-    chk2n(n);
-    chk2p(n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    chkodd_arr(arr, n);
     return 0;
 }
-
-
-
-// --------------------
-
-// // C++ program of the above approach
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// /* Function to check if x is power of 2*/
-// bool isPowerofTwo(long long n)
-// {
-// 	return (n != 0) && ((n & (n - 1)) == 0);
-// }
-
-// // Driver code
-// int main()
-// {
-// 	// Function call
-// 	isPowerofTwo(30) ? cout << "Yes\n" : cout << "No\n";
-// 	isPowerofTwo(128) ? cout << "Yes\n" : cout << "No\n";
-// 	return 0;
-// }
-
-
-// ------------------------
-
-// // C++ Program to find whether a
-// // no is a power of two
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// // Function to check if x is power of 2
-// bool isPowerOfTwo(int n)
-// {
-// 	if (n == 0)
-// 		return false;
-
-// 	return (ceil(log2(n)) == floor(log2(n)));
-// }
-
-// // Driver code
-// int main()
-// {
-// 	// Function call
-// 	isPowerOfTwo(31) ? cout << "Yes" << endl
-// 					: cout << "No" << endl;
-// 	isPowerOfTwo(64) ? cout << "Yes" << endl
-// 					: cout << "No" << endl;
-
-// 	return 0;
-// }
