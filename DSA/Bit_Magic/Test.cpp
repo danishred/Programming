e419923493d6ac69d8c47f385af9fe01b04f51c3
danishred <83757129+danishred@users.noreply.github.com>
2023-08-15 01:18:03 +0530

    init

--- DSA/Bit_Magic/Test.cpp
+++ DSA/Bit_Magic/Test.cpp
@@ -1,35 +1,70 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
+// 
+        
+#include <iostream> 
 using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
 
 class Solution
 {
 public:
-    // Function to find number of bits needed to be flipped to convert A to B
-    int countBitsFlip(int a, int b)
-    {
 
-        // Your logic here
-    }
+// 
+int (int n)
+{
+
+}
+// Time Complexity:
+// Auxiliary Space:
+
 };
-
-//{ Driver Code Starts.
 
 // Driver Code
 int main()
 {
 
-    int a, b;
-    cin >> a >> b; // input a and b
+    int a;
+    cin >> a; // input a and b
 
     Solution ob;
-    cout << ob.countBitsFlip(a, b) << endl;
+    ob.(a) ;
 
     return 0;
 }
-// } Driver Code Ends
+
+
+
+
+
+
+
+
+// 
+        
+#include <iostream> 
+using namespace std;
+
+class Solution
+{
+public:
+
+// 
+int (int n)
+{
+
+}
+// Time Complexity:
+// Auxiliary Space:
+
+};
+
+// Driver Code
+int main()
+{
+
+    int a;
+    cin >> a; // input a and b
+
+    Solution ob;
+    ob.(a) ;
+
+    return 0;
+}
