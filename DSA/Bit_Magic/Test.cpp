bc8f475318fc1787fd1e8f7672b959f45ba2a898
danishred <83757129+danishred@users.noreply.github.com>
2023-08-16 20:17:33 +0530

    init

--- DSA/Bit_Magic/Test.cpp
+++ DSA/Bit_Magic/Test.cpp
@@ -1,28 +1,10 @@
 //
 
 #include <iostream>
-#include "supplement.h"
-#include <cstring>
-#include <math.h>
 using namespace std;
-
-void powerset(char str[])
-{
-    int n = strlen(str);
-    int psets = 1 << n;
-
-    for (int i = 0; i < psets; i++)
-    {
-        /* code */
-    }
-}
 
 int main()
 {
-    int n;
-    cin >> n;
-    char str[n];
-    cin >> str;
-    powerset(str);
+    
     return 0;
 }
--- DSA/Bit_Magic/power_set.cpp
+++ DSA/Bit_Magic/power_set.cpp
@@ -1,4 +1,4 @@
-//
+// Program to print powersets of a string
 
 #include <iostream>
 #include "supplement.h"
@@ -6,22 +6,36 @@
 #include <math.h>
 using namespace std;
 
+// Sir's
 void powerset(char str[])
 {
     int n = strlen(str);
-    int psets = 1 << n;
 
+    int psets = 1 << n;
+    cout << "\n"
+         << psets << endl;
+
+    // no. of powersets produced
     for (int i = 0; i < psets; i++)
     {
-        /* code */
+        // every bit is being checked whether it is set or not
+        for (int j = 0; j < n; j++)
+        {
+            // if set then printed
+            if ((i & (1 << j)) != 0)
+            {
+                cout << str[j];
+            }
+        }
+        cout << endl;
     }
 }
+// Time Complexity:Θ(2^n*n)
+// Auxiliary Space:Θ(1)
 
 int main()
 {
-    int n;
-    cin >> n;
-    char str[n];
+    char str[10];
     cin >> str;
     powerset(str);
     return 0;
