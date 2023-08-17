6d6b5aed4ede1d82c297b53a771d078c1e253b8e
danishred <83757129+danishred@users.noreply.github.com>
2023-08-17 18:44:19 +0530

    init

--- DSA/Bit_Magic/Swap_odd_even_bits.cpp
+++ DSA/Bit_Magic/Swap_odd_even_bits.cpp
@@ -1,6 +1,7 @@
 // Swap all odd and even bits
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void swap(unsigned int n)
@@ -26,8 +27,6 @@ void swap(unsigned int n)
         }
         res = res | x;
 
-
-
         y = n;
         y = y >> i + 1;
 
@@ -48,10 +47,42 @@ void swap(unsigned int n)
          << res << endl;
 }
 
+// Function to swap odd and even bits
+// GFGs
+void swapBits(unsigned int n)
+{
+    // Your code here
+    vector<int> v;
+    while (n > 0)
+    {
+        v.push_back(n % 2);
+        n = n >> 1;
+    }
+    v.push_back(0);
+    for (int i = 0; i < v.size() - 1; i += 2)
+    {
+        swap(v[i], v[i + 1]);
+    }
+    unsigned int ans = 0;
+    unsigned int bit = 1;
+    for (int i = 0; i < v.size(); i++)
+    {
+        ans += bit * v[i];
+        bit *= 2;
+    }
+    cout << endl
+         << ans << endl;
+}
+// Time Complexity: O(logN), 
+// Conversion of decimal to binary and binary to decimal takes logN time.
+// Auxiliary Space: O(logN),
+// LogN space takes vector to store the bits of given number.
+
 int main()
 {
     unsigned int n;
     cin >> n;
     swap(n);
+    swapBits(n);
     return 0;
 }
