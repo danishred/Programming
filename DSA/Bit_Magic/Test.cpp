52d98fa6c7e57625b63db6a0f841990b1d42e79c
danishred <83757129+danishred@users.noreply.github.com>
2023-08-21 22:40:19 +0530

    init

--- DSA/Bit_Magic/Test.cpp
+++ DSA/Bit_Magic/Test.cpp
@@ -1,51 +1,26 @@
-// Given a number N. The task is to check
-// whether it is sparse or not. A number is said to
-// be a sparse number if no two or more consecutive
-//  bits are set in the binary representation.
+// Find gray code of the decimal N and
+// convert it into decimal
 
 #include <iostream>
 using namespace std;
 
-// Mine loop
-int isSparse(int n)
+int greyConverter(int n)
 {
-    int count = 0;
-    for (int i = 0; i < 31; i++)
+    int res = 0;
+    for (int i = 31; i >= 0; i--)
     {
-        if ((n & 1) == 1)
-            count++;
-        else
-            count = 0;
-        if (count > 1)
-        {
-            // Not sparse
-            return 0;
-        }
-        n = n >> 1;
+        if (((n >> i) & 1) == 1)
+            {
+                
+            }
+        n = n >> i
     }
-    // Sparse
-    return 1;
 }
-// Time Complexity:O(1)
-// Auxiliary Space:O(1)
-
-// GFG
-bool isSparse2(int n)
-{
-    // we perform Right shift on n by 1 bit.
-    // then perform AND operation on n and n/2
-    //(obtained by right shifting n by 1 bit).
-    // returning true if we get 0 as result otherwise false.
-    return (n & (n >> 1)) == 0;
-}
-// Time Complexity:O(1)
-// Auxiliary Space:O(1)
 
 int main()
 {
     int n;
     cin >> n;
-    cout << isSparse(n) << endl;
-    cout << isSparse2(n) << endl;
+    cout << greyConverter(n) << endl;
     return 0;
 }
