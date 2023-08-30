b827d38aa0d88219c508fbd7e8014afaed2bf174
danishred <83757129+danishred@users.noreply.github.com>
2023-08-30 16:45:53 +0530

    init

--- DSA/Recursion/Test.cpp
+++ DSA/Recursion/Test.cpp
@@ -1,24 +1,30 @@
-//
+// Recursive program to check for palindrome
 
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-//
+// Mine
 bool recChkPalin(char str[], int n, int k)
 {
-    if (str)
-    {
-        /* code */
-    }
+    if (n <= k)
+        return true;
+    if (str[n] == str[k])
+       return recChkPalin(str, n - 1, k + 1);
+    else
+        return false;
+    return 1;
 }
+// Time Complexity: O(n) 
+// Auxiliary Space: O(n)
+
 
 int main()
 {
     int n, k = 0;
     char str[100];
-    n = strlen(str);
     cin >> str;
-    cout << recChkPalin(str, n, k) << endl;
+    n = strlen(str);
+    cout << recChkPalin(str, n-1, k) << endl;
     return 0;
 }
