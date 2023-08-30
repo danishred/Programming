6f768c2204e006179921a6f0f1d8eb85a85b1a89
danishred <83757129+danishred@users.noreply.github.com>
2023-08-30 18:18:13 +0530

    init

--- DSA/Recursion/Test.cpp
+++ DSA/Recursion/Test.cpp
@@ -10,13 +10,13 @@ bool recChkPalin(char str[], int n, int k)
     if (n <= k)
         return true;
     if (str[n] == str[k])
-       return recChkPalin(str, n - 1, k + 1);
+        return recChkPalin(str, n - 1, k + 1);
     else
         return false;
     return 1;
 }
-// Time Complexity: O(n) 
-// Auxiliary Space: O(n)
+// Time Complexity: O(n) ( Θ(n) in the worst case)
+// Auxiliary Space: O(n) ( Θ(n) in the worst case)
 
 
 int main()
@@ -25,6 +25,29 @@ int main()
     char str[100];
     cin >> str;
     n = strlen(str);
-    cout << recChkPalin(str, n-1, k) << endl;
+    cout << recChkPalin(str, n - 1, k) << endl;
     return 0;
 }
+
+// Sir's
+// #include <iostream>
+// using namespace std;
+
+
+// bool isPalindrome(string str, int start, int end)
+// {
+// 	if(start >= end)
+// 		return true;
+
+// 	return ((str[start]==str[end]) && 
+// 		     isPalindrome(str, start + 1, end - 1));
+// }
+
+// int main() {
+	
+// 	string s = "GeekskeeG";
+	
+// 		printf("%s", isPalindrome(s, 0, s.length() -1) ? "true" : "false");
+	
+// 	return 0;
+// }
