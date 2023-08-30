fea27c5e198aa0101c86d161c5eda712ae49d993
danishred <83757129+danishred@users.noreply.github.com>
2023-08-30 10:28:25 +0530

    init

--- DSA/Recursion/Test.cpp
+++ DSA/Recursion/Test.cpp
@@ -3,7 +3,19 @@
 #include <iostream>
 using namespace std;
 
+// Head recursive
 int fact(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * fact(n - 1);
+}
+// Time Complexity:Θ(n)
+// Auxiliary Space: Θ(n)
+
+// Tail recursive
+
+int fact1(int n)
 {
     if (n == 0 || n == 1)
         return 1;
@@ -17,5 +29,6 @@ int main()
     int n;
     cin >> n;
     cout << fact(n) << endl;
+    cout << fact1(n) << endl;
     return 0;
 }
