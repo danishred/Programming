b411e26fdcf160f14ac27f609a293138e684d91f
danishred <83757129+danishred@users.noreply.github.com>
2023-08-30 14:44:25 +0530

    init

--- DSA/Recursion/Test.cpp
+++ DSA/Recursion/Test.cpp
@@ -1,33 +1,23 @@
-// Print all numbers upto n
+// Print sum of all numbers upto n
 
 #include <iostream>
 using namespace std;
 
 // Head recursive
-int fact(int n)
+int sum(int n)
 {
-    if (n == 0 || n == 1)
-        return 1;
-    return n * fact(n - 1);
+    if (n <= 0)
+        return 0;
+    return n + sum(n - 1);
 }
 // Time Complexity:Θ(n)
 // Auxiliary Space: Θ(n)
 
-// Tail recursive
-int fact1(int n, int k)
-{
-    if (n == 0 || n == 1)
-        return k;
-    return fact1(n - 1, k *(n));
-}
-// Time Complexity: Θ(n)
-// Auxiliary Space: O(1)
-
 int main()
 {
-    int n, k = 1;
+    int n;
     cin >> n;
-    cout << fact(n) << endl;
-    cout << fact1(n, k) << endl;
+    cout << sum(n) << endl;
+
     return 0;
 }
