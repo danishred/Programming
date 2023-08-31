8f4c8dee0833c7ef59ab0cc466612c360f770fe2
danishred <83757129+danishred@users.noreply.github.com>
2023-08-31 22:52:46 +0530

    init

--- DSA/Recursion/towerOfHanoi.cpp
+++ DSA/Recursion/towerOfHanoi.cpp
@@ -6,11 +6,14 @@ void TOH(int n, char A, char B, char C)
 {
     if (n == 1)
     {
-        cout << "Move 1 from " << A << " to " << C << endl;
+        // B as Auxiliary
+        cout << "Move 1 from " << A << " to " << C << endl; 
         return;
     }
+    // C as Auxiliary
     TOH(n - 1, A, C, B);
     cout << "Move " << n << " from " << A << " to " << C << endl;
+    // A as Auxiliary
     TOH(n - 1, B, A, C);
 }
 
