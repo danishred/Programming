adb5d5b7a046f9a383453854b152f343a3557fdd
danishred <83757129+danishred@users.noreply.github.com>
2023-09-03 20:44:27 +0530

    init

--- DSA/3_Recursion/tempCodeRunnerFile.cpp
+++ DSA/3_Recursion/tempCodeRunnerFile.cpp
@@ -1,4 +1,37 @@
-    // for (auto &x : arr)
-    // {
-    //     cout << x << endl;
-    // }
+
+		int k = 0;
+		int sum = 0;
+		int min = INT_MAX;
+
+		for (int i = 0; i < n; i++)
+		{
+			sum = sum + A[i];
+		}
+		if ((sum & 1) == 0)
+		{
+			return 0;
+		}
+
+		else
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if ((n & A[i]) == 0 && (A[i] < min))
+				{
+					min = A[i];
+				}
+			}
+
+			if (min == INT_MAX)
+			{
+				return -1;
+			}
+
+			while ((min & 1) != 1)
+			{
+				min = min / 2;
+				k++;
+			}
+
+			return k;
+		}
