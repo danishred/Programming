497a296dfbabd839f8898b52649430116c31997f
danishred <83757129+danishred@users.noreply.github.com>
2023-09-03 11:48:44 +0530

    init

--- DSA/3_Recursion/subsetSum.cpp
+++ DSA/3_Recursion/subsetSum.cpp
@@ -45,3 +45,23 @@ int main()
          << endl;
     return 0;
 }
+
+
+// // Sirs
+// int countSubsets(int arr[], int n, int sum)
+// {
+// 	if(n==0)
+// 		return sum==0? 1 : 0;
+
+// 	return countSubsets(arr, n-1, sum) + countSubsets(arr, n-1, sum - arr[n-1]);
+// }
+
+
+// int main() {
+    
+//     	int n = 2, arr[]= {3,4}, sum = 7;
+    	
+//     	cout<<countSubsets(arr, n, sum);
+    	
+//     	return 0;
+// }
