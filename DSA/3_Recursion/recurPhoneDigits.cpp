1de170042eebe1662ea518e1cf7296e116d568a6
danishred <83757129+danishred@users.noreply.github.com>
2023-09-05 10:13:21 +0530

    init

--- DSA/3_Recursion/recurPhoneDigits.cpp
+++ DSA/3_Recursion/recurPhoneDigits.cpp
@@ -26,16 +26,20 @@ public:
 		mp[9] = "wxyz";
 	}
 
+
 	void fun(string s, int a[], int N, int index)
 	{
-		string tmp = mp[a[index]];
-		for (int j = index; j < N; j++)
+		if (index == N)
 		{
-			/* code */
+			ans.push_back(s);
 		}
-		
-		ans.push_back();
-		fun(s, a, N, index + 1);
+		string tmp = mp[a[index]];
+
+		for (int j = 0; j < tmp.length(); j++)
+		{
+			fun(s + tmp[j], a, N, index + 1);
+		}
+		return ;
 	}
 
 	// Function to find list of all words possible by pressing given numbers.
@@ -52,13 +56,14 @@ public:
 
 int main()
 {
-
+	cout << "Enter number of keypress: " << endl;
 	int N;
 
 	cin >> N; // input size of array
 
 	int a[N]; // declare the array
 
+	cout << "Enter keys being pressed: " << endl;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> a[i]; // input the elements of array that are keys to be pressed
