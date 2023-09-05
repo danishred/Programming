9f43dcf5df1efac98ff3d58859d393ea1c4048d6
danishred <83757129+danishred@users.noreply.github.com>
2023-09-05 06:22:56 +0530

    init

--- DSA/3_Recursion/recurPhoneDigits.cpp
+++ DSA/3_Recursion/recurPhoneDigits.cpp
@@ -1,49 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for C++
 
 #include <bits/stdc++.h>
 #include <string>
 
 using namespace std;
 
-
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 class Solution
 {
-    public:
-    //Function to find list of all words possible by pressing given numbers.
-    vector<string> possibleWords(int a[], int N)
-    {
-        //Your code here
-    }
-};
+public:
+	vector<string> ans;
+	unordered_map<int, string> mp;
+	Solution()
+	{
+		mp[2] = "abc";
+		mp[3] = "def";
+		mp[4] = "ghi";
+		mp[5] = "jkl";
+		mp[6] = "mno";
+		mp[7] = "pqrs";
+		mp[8] = "tuv";
+		mp[9] = "wxyz";
+	}
 
+	void fun(string s, int a[], int N, int index)
+	{
+		string tmp = mp[a[index]];
+		for (int j = index; j < N; j++)
+		{
+			/* code */
+		}
+		
+		ans.push_back();
+		fun(s, a, N, index + 1);
+	}
+
+	// Function to find list of all words possible by pressing given numbers.
+	vector<string> possibleWords(int a[], int N)
+	{
+		// Your code here
+		string s = "";
+		fun(s, a, N, 0);
+		return ans;
+	}
+};
 
 //{ Driver Code Starts.
 
-int main() {
-  
-	int T;
-	
-	cin >> T; //testcases
-	
-	while(T--){ //while testcases exist
-	   int N;
-	    
-	   cin >> N; //input size of array
-	   
-	   int a[N]; //declare the array
-	   
-	   for(int i =0;i<N;i++){
-	       cin >> a[i]; //input the elements of array that are keys to be pressed
-	   }
-	   
-	   Solution obj;
-	   
-	  vector <string> res = obj.possibleWords(a,N);
-	  for (string i : res) cout << i << " ";
-	   cout << endl;
+int main()
+{
+
+	int N;
+
+	cin >> N; // input size of array
+
+	int a[N]; // declare the array
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a[i]; // input the elements of array that are keys to be pressed
 	}
-	
+
+	Solution obj;
+
+	vector<string> res = obj.possibleWords(a, N);
+	for (string i : res)
+		cout << i << " ";
+	cout << endl;
+
 	return 0;
 }
