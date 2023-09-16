59adfe3110d93448b7cd64b10717694e78aa1c79
danishred <83757129+danishred@users.noreply.github.com>
2023-09-16 18:49:22 +0530

    init

---
+++ DSA/4_Arrays/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+
+// int maxIndex(int arr[], int n)
+// {
+//     int max = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (arr[i] <= arr[j] && i < j && j - i > max)
+//             {
+//                 max = j - i;
+//             }
+//         }
+//     }
+
+//     return max;
+// }
--- DSA/4_Arrays/test.cpp
+++ DSA/4_Arrays/test.cpp
@@ -1,30 +1,67 @@
-//
+// Max Index
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int largestsum(int arr[], int n)
+// Bruteforce
+int maxIndex(int arr[], int n)
 {
-    int max_so_far = INT_MIN;
-    int max_ending_here = 0;
+    int mx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // We can use j = i+1 to reduce some complexity
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] <= arr[j] && i < j)
+            {
+                mx = max(j - i, mx);
+            }
+        }
+    }
+
+    return mx;
+}
+
+int maxIndex1(int arr[], int n)
+{
+    int max = 0;
+    // current max (in maxright) or min (in minleft) is checked
+    int maxright[n];
+    int minleft[n];
+
+    maxright[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (maxright[i - 1] > arr[i])
+        {
+            maxright[i] = maxright[i - 1];
+        }
+        else
+            maxright[i] = arr[i];
+    }
+
+    minleft[n - 1] = arr[n - 1];
+    for (int i = n - 2; i > 0; i--)
+    {
+        if (minleft[i + 1] > arr[i])
+        {
+            
+        }
+    }
 
     for (int i = 0; i < n; i++)
     {
-        max_ending_here += arr[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
-
-        if (max_ending_here < 0)
-            max_ending_here = 0;
+        cout << maxright[i] << " ";
     }
 
-    return max_so_far;
+    return max;
 }
 
 int main()
 {
-    int arr[] = {-3, 4, -1, -2, 1, 5}, n = 6;
-    int res = largestsum(arr, n);
-    cout << res << endl;
+    int arr[] = {34, 8, 10, 3, 2, 80, 30, 33, 1}, n = 9;
+    // int res = maxIndex(arr, n);
+    maxIndex1(arr, n);
+    // cout << res << endl;
     return 0;
 }
