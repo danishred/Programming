9d9f897b8dfdd543d9465da3caae0def85d41fcb
danishred <83757129+danishred@users.noreply.github.com>
2023-09-19 07:43:41 +0530

    init

--- DSA/4_Arrays/test.cpp
+++ DSA/4_Arrays/test.cpp
@@ -1,38 +1,24 @@
-// Second Largest element
-#include <iostream>
-#include <climits>
+//
 
+#include <iostream>
 using namespace std;
 
-int secondLargest(int arr[], int n)
+int checkSort(int arr[], int n)
 {
-    // second value
-    int scnd = INT_MIN;
-    // first and second max index
-    int mx = 0, mx2 = -1;
-    for (int i = 0; i < n; i++)
+    if (n == 1)
+        return true;
+    for (int i = 1; i < n; i++)
     {
-        if (arr[mx] < arr[i])
-        {
-            mx = i;
-        }
+        if (arr[i - 1] > arr[i])
+            return false;
     }
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[mx]>arr[i])
-        {
-            if (scnd < arr[i])
-            {
-                scnd = arr[i];
-                mx2 = i;
-            }
-        }
-    }
-    return mx2;
+    return true;
 }
+
 int main()
 {
-    int arr[] = {10,10,10}, n = 3;
-    cout << secondLargest(arr, n) << endl;
+    int n = 3;
+    int arr[] = {100,20,200};
+    cout << checkSort(arr, n) << endl;
     return 0;
 }
