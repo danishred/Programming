f79896d9f9cd22ddfbfa74674d2a5d94f05e6f25
danishred <83757129+danishred@users.noreply.github.com>
2023-09-22 08:32:25 +0530

    init

--- DSA/4_Arrays/test.cpp
+++ DSA/4_Arrays/test.cpp
@@ -1,55 +1,32 @@
-// Left rotate(anticlockwise) array by one
-
+// Find leaders in array
 #include <iostream>
 using namespace std;
 
-void rotateLeft1(int arr[], int n, int d)
+// Sir's
+void leaders(int arr[], int n)
 {
-    int temp[d];
-
-    for (int j = 0; j < d; j++)
+    for (int i = 0; i < n; i++)
     {
-        temp[j] = arr[j];
-    }
-
-    for (int i = d; i < n; i++)
-    {
-        arr[i - d] = arr[i];
-    }
-
-    for (size_t i = 0; i < count; i++)
-    {
-        /* code */
-    }
-    
-}
-// Time Complexity:Θ(n)
-// Auxiliary Space:Θ(d)
-void rotateLeft(int arr[], int n, int d)
-{
-    for (int j = 0; j < d; j++)
-    {
-        int temp = arr[0];
-        for (int i = 1; i < n; i++)
+        for (int j = i + 1; j < n; j++)
         {
-            arr[i - 1] = arr[i];
+            if (arr[i] <= arr[j])
+                break;
+
+            if (j == n - 1)
+                cout << arr[i]<<" ";
         }
-        arr[n - 1] = temp;
     }
+    cout<<arr[n-1];
 }
-// Time Complexity:Θ(dn)
-// Auxiliary Space:Θ(1)
+// Time Complexity:O(n^2) 
+// Auxiliary Space:O(1 )
+
 
 int main()
 {
-    int n = 5, d = 2;
-    int arr[] = {1, 2, 3, 4, 5};
-    // rotateLeft(arr, n, d);
-    rotateLeft1(arr, n, d);
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    int n = 7;
+    int arr[] = {7, 10, 4, 10, 6, 5, 2};
+    leaders(arr, n);
     cout << endl;
     return 0;
 }
