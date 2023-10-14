af68214117c99cf92ffb9670c8ba23517ab73873
danishred <83757129+danishred@users.noreply.github.com>
2023-10-14 07:15:02 +0530

    init

--- DSA/Zextras/vectors.cpp
+++ DSA/Zextras/vectors.cpp
@@ -10,21 +10,35 @@ int main()
     // n for inputing no. of elements into vector using for loop
     // a for taking user input and pushing back into vectors
     int n, a;
-    cin >> n;
+    // can use value of n or itterator for accessing values
+    // Using itterator we don't need to know the size of vector
+    // cin >> n;
     // datastructure<type> varname;
     // No need of[] brackets
-    vector<int> arr;
+    // 5 instance of 10
+    vector<int> arr(5, 10);
     vector<int> curr;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a;
-        // to add add back an element
-        arr.push_back(a);
-    }
-    // use & for vectors
-    for (auto &x : arr)
-    {
-        cout << x << endl;
-    }
+
+    // can use same methodology as arrays to change and access values
+    // cout << arr[3] << endl;
+    // arr[3] = 5;
+    // cout << arr[3] << endl;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a;
+    //     // to add add back an element
+    //     arr.push_back(a);
+    // }
+    // // use & for vectors
+    // for (auto &x : arr)
+    // {
+    //     cout << x << endl;
+    // }
+
+    // Itterator method
+    vector<int> k = {3, 4, 5};
+    // Well k.back() returns value while k.begin() returns addresss
+    cout<<k.back()<<endl<<*(k.begin())<<endl;
     return 0;
 }
