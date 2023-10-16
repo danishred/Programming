93312b8534dae1b1f1b88cfb63b3b0130700a8b1
danishred <83757129+danishred@users.noreply.github.com>
2023-10-16 22:02:56 +0530

    init

--- DSA/Zextras/upperAndLowerbound.cpp
+++ DSA/Zextras/upperAndLowerbound.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-    int a[]={1,4,6,9,9};
+    int a[]={1,4,6,9,9,11,13};
     int n = sizeof(a)/sizeof(int);
     int ind = lower_bound(a,a+n,4)-a;
-    int inde = upper_bound(a,a+n,4)-a;
+    int inde = upper_bound(a,a+n,9)-a;
     cout<<ind<<endl; // 1 
     cout<<inde<<endl; // 2
     return 0;
