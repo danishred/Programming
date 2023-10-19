17130544b2de1f4ce41dd04d00e8a3c6db9f7eb4
danishred <83757129+danishred@users.noreply.github.com>
2023-10-19 17:44:28 +0530

    init

--- DSA/4_Arrays/test.cpp
+++ DSA/4_Arrays/test.cpp
@@ -1,92 +1,66 @@
-//{ Driver Code Starts
-//Initial template for C++
+// Trapping rain water
 
-#include <bits/stdc++.h>
+#include <iostream>
+// #include <math.h>
 using namespace std;
 
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution{
-public:
-    //Function to find the days of buying and selling stock for max profit.
-    vector<vector<int> > stockBuySell(vector<int> A, int n)
-    {
-        vector<vector<int> > arr;
-        
-        int buy = 0, sell = 0;
-
-        if (n == 1)
-            return 0;
-    
-        for (int i = 0; i < n; i++)
-        {
-            if (i - 1 < 0 && arr[i] < arr[i + 1])
-            {
-                buy = arr[i];
-                continue;
-            }
-            if (i - 1 < 0)
-            {
-                continue;
-            }
-            if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1])
-            {
-                sell = sell + arr[i] - buy;
-                continue;
-            }
-            if (arr[i - 1] > arr[i] && arr[i] < arr[i + 1])
-            {
-                buy = arr[i];
-                // sell = sell - arr[i];
-                continue;
-            }
-            if (arr[i - 1] < arr[i] && i + 1 > n - 1)
-                sell = sell + arr[i] - buy;
-        }
-        return sell;
-    }
-};
-
-//{ Driver Code Starts.
-
-int check(vector<vector<int>> ans, vector<int> A, int p)
+int getWater(int arr[], int n)
 {
-    int c = 0;
-    for(int i=0; i<ans.size(); i++)
-        c += A[ans[i][1]]-A[ans[i][0]];
-    return (c==p) ? 1 : 0;
+    int water = 0, wallval = arr[0], mwall_val = arr[0], wallid = 0,
+        mwallid = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > wallval && mwallid != wallid)
+        {
+            water += ((min(arr[i],mwall_val))-wallval) * (i - mwallid - 1);
+        }
+        if (arr[i] >= mwall_val)
+        {
+            mwall_val = arr[i];
+            mwallid = i;
+        }
+        if (arr[i] > 0)
+        {
+            water += (i - wallid - 1) * (min(wallval, arr[i]));
+            wallval = arr[i];
+            wallid = i;
+        }
+    }
+    return water;
+}
+
+int trap(int arr[], int n)
+{
+    int water = 0, wall_val = 0, wall_idx = 0, tmp_water = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= wall_val)
+        {
+            wall_val = arr[i];
+            water += tmp_water;
+            tmp_water = 0;
+            wall_idx = i;
+            continue;
+        }
+        if (i == (n - 1))
+        {
+            tmp_water = arr[i] * (n - 1 - wall_idx);
+            water += tmp_water;
+        }
+        else
+        {
+            tmp_water += wall_val - arr[i];
+        }
+    }
+
+    return water;
 }
 
 int main()
-{   
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> A(n);
-        for (int i=0; i<n; ++i){
-            cin>>A[i];
-        }
-        Solution ob;
-        vector<vector<int> > ans = ob.stockBuySell(A, n);
-        int p = 0;
-        for(int i=0; i<n-1; i++)
-        {
-            int x = A[i+1]-A[i];
-            if(x>0)
-                p += x;
-        }
-        if(ans.size()==0)
-            cout<<"No Profit";
-        else{
-            cout<<check(ans,A,p);
-        }cout<<endl;
-    }
+{
+    int arr[] = {3,0,1,2,5};
+    int n = sizeof(arr) / sizeof(int);
+    cout << getWater(arr, n) << endl;
     return 0;
 }
-
-
-// } Driver Code Ends
