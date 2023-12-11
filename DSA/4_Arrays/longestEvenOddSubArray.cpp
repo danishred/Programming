6700ec9825ef235c3777fb831fd7ccb2fe2a8b95
danishred <83757129+danishred@users.noreply.github.com>
2023-12-11 11:20:33 +0530

    init

--- DSA/4_Arrays/longestEvenOddSubArray.cpp
+++ DSA/4_Arrays/longestEvenOddSubArray.cpp
@@ -1,4 +1,5 @@
-//{ Driver Code Starts
+// Longest Even Odd Subarray
+// { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,36 +11,38 @@ public:
     int maxEvenOddSubArray(int arr[], int n)
     {
         // code here
-        int result = 1, // keeps the max 
-        odd_flag = 0,   // whether last element was odd or not
-         = 0;      // count for current longest even-odd subarray
+        int maxCount = 1,            // keeps the longest count found yet
+            odd_flag = 0,            // whether last element was odd or not
+            currentLongestCount = 0; // count for current longest even-odd sub-array
+
         for (int i = 0; i < n; i++)
         {
-            if (odd_flag == 0)
+            if (odd_flag == 0) // if last element was even
+
             {
-                if (arr[i] & 1) // if current number is odd
+                if (arr[i] & 1) // if current element is odd
                 {
-                    odd_flag = 1; // switch flag to 1
-                    count++; // increment count
-                    result = max(result, count); // store current count if its high 
+                    odd_flag = 1;                                  // switch flag to 1
+                    currentLongestCount++;                         // increment count
+                    maxCount = max(maxCount, currentLongestCount); // store current count if its higher
                 }
                 else
-                    count = 1;
+                    currentLongestCount = 1; // reset current longest count
             }
 
-            else
+            else // if last element was odd
             {
-                if ((arr[i] & 1) == 0)
+                if ((arr[i] & 1) == 0) // if current element is even
                 {
-                    odd_flag = 0;
-                    count++;
-                    result = max(result, count);
+                    odd_flag = 0;                                  // switch flag to 0
+                    maxCount++;                                    // increment count
+                    maxCount = max(maxCount, currentLongestCount); // store current count if its higher
                 }
                 else
-                    count = 1;
+                    currentLongestCount = 1; // reset current longest count
             }
         }
-        return result;
+        return maxCount;
     }
     // Time Complexity:O(n^2)
     // Auxiliary Space:O(1)
@@ -48,7 +51,7 @@ public:
 //{ Driver Code Starts.
 int main()
 {
-    int arr[] = {10,12,8,4};
+    int arr[] = {10, 12, 14, 7, 8};
     int n = sizeof(arr) / sizeof(int);
     // cout << n << endl;
     Solution ob;
