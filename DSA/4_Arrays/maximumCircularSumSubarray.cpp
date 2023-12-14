1e1d52fe2220a6d77f3eaae471e4395722866435
danishred <83757129+danishred@users.noreply.github.com>
2023-12-14 21:31:46 +0530

    init

--- DSA/4_Arrays/maximumCircularSumSubarray.cpp
+++ DSA/4_Arrays/maximumCircularSumSubarray.cpp
@@ -115,30 +115,6 @@ public:
     }
     // Time Complexity: O(n^2)
     // Auxiliary Space: O(1)
-
-    // Sir's Naive
-    int maxCircularSum(int arr[], int n)
-    {
-        int res = arr[0];
-
-        for (int i = 0; i < n; i++)
-        {
-            int curr_max = arr[i];
-            int curr_sum = arr[i];
-
-            for (int j = 1; j < n; j++)
-            {
-                int index = (i + j) % n;
-
-                curr_sum += arr[index];
-
-                curr_max = max(curr_max, curr_sum);
-            }
-
-            res = max(res, curr_max); // I think the code will still work without this line (test)
-        }
-        return res;
-    }
 };
 
 // Driver Code
