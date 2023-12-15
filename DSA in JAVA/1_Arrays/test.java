0495481c7656e79bba77bd61e527be4d8747914d
danishred <83757129+danishred@users.noreply.github.com>
2023-12-16 00:40:18 +0530

    init

---
+++ DSA in JAVA/1_Arrays/test.java
@@ -0,0 +1,15 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        // numbers can be printed with or without quotes
+        // strings cannot
+        System.out.println(1);
+        System.out.println("1");
+        // operations can also be performed this way
+        System.out.println(2+1);
+        System.out.println(3*4);
+    }
+}
