74a0033ab380b69ced6d225c15b9a45589f7ca69
danishred <83757129+danishred@users.noreply.github.com>
2023-12-23 18:49:35 +0530

    init

--- DSA in JAVA/4_Arrays/waveArray.java
+++ DSA in JAVA/4_Arrays/waveArray.java
@@ -28,7 +28,7 @@ class IntArray {
     }
 }
 
-class gfg {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n;
