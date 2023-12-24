6ea843dcb8f51087542c54875db1c5fc9db6e241
danishred <83757129+danishred@users.noreply.github.com>
2023-12-24 18:53:29 +0530

    init

--- DSA in JAVA/4_Arrays/test.java
+++ DSA in JAVA/4_Arrays/test.java
@@ -1,61 +1,35 @@
-import java.io.*;
-import java.util.*;
-import java.util.Arrays;
+import java.util.Scanner;
 
-class IntArray {
-    public static int[] input(BufferedReader br, int n) throws IOException {
-        String[] s = br.readLine().trim().split(" ");
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(s[i]);
+/**
+ * test
+ */
+public class test {
 
-        return a;
+    public static int majorityElement(int[] arr, int n) {
+        int k = n / 2, count = 0, majority = 0, num = -1;
+        for (int i : arr) {
+            for (int j : arr) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if ((count > k) && (count > majority)) {
+                majority = count;
+                num = i;
+            }
+            count = 0;
+        }
+        return num;
     }
 
-    public static void print(int[] a) {
-        for (int e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-
-    public static void print(ArrayList<Integer> a) {
-        for (int e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    public static void main(String[] args) {
+        Scanner val = new Scanner(System.in);
         int n;
-        n = Integer.parseInt(br.readLine());
+        int[] arr;
+        n = val.nextInt();
+        System.out.println(n);
 
-        int[] a = IntArray.input(br, n);
-
-        Solution obj = new Solution();
-        obj.convertToWave(n, a);
-        StringBuffer sb = new StringBuffer("");
-        for (int i : a) {
-            sb.append(i + " ");
-        }
-        System.out.println(sb.toString());
+        // int[] arr = new int[n];
 
     }
 }
-
-// } Driver Code Ends
-
-class Solution {
-    public static void convertToWave(int n, int[] arr) {
-        // code here
-        int temp;
-        for (int i = 0; i < n - 1; i += 2) {
-            temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
-        }
-    }
-
-};
